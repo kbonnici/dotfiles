1d304001daf3d689a2cd5c5fb3e9fa90112f13a0
Karl Bonnici <karl.bonnici100@gmail.com>
2023-03-16 13:53:37 -0700

    remove redundent mappings

--- neovim/.config/nvim/lua/kbonnici/plugin/nvim_cmp.lua
+++ neovim/.config/nvim/lua/kbonnici/plugin/nvim_cmp.lua
@@ -15,6 +15,13 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
     }),
 
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "path" },
+        { name = "buffer" },
+    },
+
     window = {
         completion = cmp.config.window.bordered({
             border = "rounded",
