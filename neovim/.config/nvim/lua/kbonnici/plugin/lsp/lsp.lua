3b9563e109388ec06c9865f9f0eafb5687b5ba6f
Karl Bonnici <karl.bonnici100@gmail.com>
2023-03-22 18:34:03 -0700

    fix typescript lsp error

--- neovim/.config/nvim/lua/kbonnici/plugin/lsp/lsp.lua
+++ neovim/.config/nvim/lua/kbonnici/plugin/lsp/lsp.lua
@@ -1,40 +1,41 @@
-local status, lsp = pcall(require, 'lsp-zero')
+local status, lsp = pcall(require, "lsp-zero")
 if not status then
-    print('[Error]: LSP-Zero not installed!')
-    return
+	print("[Error]: LSP-Zero not installed!")
+	return
 end
 
-
-lsp.preset('recommended')
+lsp.preset("recommended")
 
 lsp.ensure_installed({
-	'tsserver',
-	'lua_ls',
-	'eslint',
-	'rust_analyzer',
+	"tsserver",
+	"lua_ls",
+	"eslint",
+	"rust_analyzer",
 })
 
 local function command(cmd)
-    return ":" .. cmd .. "<cr>"
+	return ":" .. cmd .. "<cr>"
 end
 
 lsp.on_attach(function(_, bufnr) -- (client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
+	local opts = { buffer = bufnr, remap = false }
 	local map = vim.keymap.set
 
-    map("n", "gd", 		    command("Lspsaga peek_definition"), opts)
-    map("n", "gi", 		    command("lua vim.lsp.buf.implementation()"), opts)
-    map("n", "gf",          command("Lspsaga lsp_finder"), opts)
-    map("n", "K", 		    command("Lspsaga hover_doc"), opts)
-    map("n", "<leader>la", 	command("Lspsaga code_action"), opts)
-    map("n", "<leader>lr", 	command("Lspsaga rename"), opts)
+	map("n", "gd", command("Lspsaga peek_definition"), opts)
+	map("n", "gi", command("lua vim.lsp.buf.implementation()"), opts)
+	map("n", "gf", command("Lspsaga lsp_finder"), opts)
+	map("n", "K", command("Lspsaga hover_doc"), opts)
+	map("n", "<leader>la", command("Lspsaga code_action"), opts)
+	map("n", "<leader>lr", command("Lspsaga rename"), opts)
 
-    map("n", "gl", 		    command("Lspsaga show_line_diagnostics"), opts)
-    map("n", "gn", 	command("Lspsaga diagnostic_jump_next"), opts)
-    map("n", "gp", 	command("Lspsaga diagnostic_jump_prev"), opts)
-
+	map("n", "gl", command("Lspsaga show_line_diagnostics"), opts)
+	map("n", "gn", command("Lspsaga diagnostic_jump_next"), opts)
+	map("n", "gp", command("Lspsaga diagnostic_jump_prev"), opts)
 end)
+
 -- enable vim global variable in lua
 lsp.nvim_workspace()
+
+lsp.skip_server_setup({ "tsserver" })
 
 lsp.setup()
