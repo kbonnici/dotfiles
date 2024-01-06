6e003535e28924ed5169517c49f1d41d78da83b4
Karl Bonnici <karl.bonnici100@gmail.com>
2024-01-06 10:28:39 -0800

    add null-ls and trouble

---
+++ neovim/.config/nvim/lua/kbonnici/plugins/trouble.lua
@@ -0,0 +1,35 @@
+return {
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	config = function()
+		local trouble = require("trouble")
+		vim.keymap.set("n", "<leader>xx", function()
+			trouble.toggle()
+		end)
+		vim.keymap.set("n", "<leader>xw", function()
+            print("Showing Workspace Diagnostics")
+			trouble.toggle("workspace_diagnostics")
+		end)
+		vim.keymap.set("n", "<leader>xd", function()
+            print("Showing Doc Diagnostics")
+			trouble.toggle("document_diagnostics")
+		end)
+		vim.keymap.set("n", "<leader>xq", function()
+            print("Showing Quickfix List")
+			trouble.toggle("quickfix")
+		end)
+		vim.keymap.set("n", "<leader>xl", function()
+            print("Showing Location List")
+			trouble.toggle("loclist")
+		end)
+		vim.keymap.set("n", "gR", function()
+            print("Show References")
+			trouble.toggle("lsp_references")
+		end)
+	end,
+}
