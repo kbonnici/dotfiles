27e5e36bbb7fb2f254cd5d33dbf344895fade1d2
Karl Bonnici <karl.bonnici100@gmail.com>
2024-01-06 10:27:45 -0800

    auto install treesitter parsers

--- neovim/.config/nvim/lua/kbonnici/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/kbonnici/plugins/treesitter.lua
@@ -1,13 +1,13 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = "TSUpdate",
-    config = function () 
-    local configs = require("nvim-treesitter.configs")
+    config = function()
+        local configs = require("nvim-treesitter.configs")
         configs.setup({
-          ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "html", "css" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
+            auto_install = true,
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
         })
-    end
+    end,
 }
