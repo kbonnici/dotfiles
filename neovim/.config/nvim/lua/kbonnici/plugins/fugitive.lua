4c69e4e1911b555b20d6916f87dfeb22ed5dcc0f
Karl Bonnici <karl.bonnici100@gmail.com>
2024-02-07 17:10:50 -0800

    add new git mappings

--- neovim/.config/nvim/lua/kbonnici/plugins/fugitive.lua
+++ neovim/.config/nvim/lua/kbonnici/plugins/fugitive.lua
@@ -2,5 +2,8 @@ return {
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>gg", "<cmd>G<cr>")
+		vim.keymap.set("n", "<leader>gp", "<cmd>G pull<cr>")
+		vim.keymap.set("n", "<leader>gP", "<cmd>G push<cr>")
+		vim.keymap.set("n", "<leader>gc", "<cmd>G commit<cr>")
 	end,
 }
