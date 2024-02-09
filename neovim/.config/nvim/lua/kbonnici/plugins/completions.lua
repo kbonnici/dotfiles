fb2bced3cb39ae556417493f9e33eb09ee33fd33
Karl Bonnici <karl.bonnici100@gmail.com>
2024-02-09 13:30:51 -0800

    i think i finally fixed snippet navigation

--- neovim/.config/nvim/lua/kbonnici/plugins/completions.lua
+++ neovim/.config/nvim/lua/kbonnici/plugins/completions.lua
@@ -32,6 +32,13 @@ return {
 					end
 				end,
 			})
+
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
 			cmp.setup({
 				snippet = {
 					expand = function(args)
@@ -48,9 +55,27 @@ return {
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-					["<C-m>"] = cmp.mapping(function() -- jump to next part of snippet
-						if ls.expand_or_jumpable() then
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+							-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+							-- that way you will only jump inside the snippet region
+						elseif ls.expand_or_jumpable() then
 							ls.expand_or_jump()
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif ls.jumpable(-1) then
+							ls.jump(-1)
+						else
+							fallback()
 						end
 					end, { "i", "s" }),
 				}),
