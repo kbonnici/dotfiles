74a93b94cdfb4b38e82f7d68416494043645eb9f
Karl Bonnici <karl.bonnici100@gmail.com>
2024-03-03 17:20:28 -0800

    nvim: install go lsp via mason + formatters

--- neovim/.config/nvim/lua/kbonnici/plugins/none-ls.lua
+++ neovim/.config/nvim/lua/kbonnici/plugins/none-ls.lua
@@ -1,14 +1,16 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
 
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettierd,
-                null_ls.builtins.diagnostics.eslint_d,
-            },
-        })
-    end,
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.formatting.gofumpt,
+				null_ls.builtins.formatting.goimports_reviser,
+			},
+		})
+	end,
 }
