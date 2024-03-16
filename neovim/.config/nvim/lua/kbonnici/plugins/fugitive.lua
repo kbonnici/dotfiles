25c39b518c153ad5d6f1b15836c1734392c20ec2
Karl Bonnici <karl.bonnici100@gmail.com>
2024-03-15 18:50:29 -0700

    nvim: replace fugitive with neogit

--- neovim/.config/nvim/lua/kbonnici/plugins/fugitive.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"tpope/vim-fugitive",
-	config = function()
-		vim.keymap.set("n", "<leader>gg", "<cmd>G<cr>")
-		vim.keymap.set("n", "<leader>gp", "<cmd>G pull<cr>")
-		vim.keymap.set("n", "<leader>gP", "<cmd>G push<cr>")
-		vim.keymap.set("n", "<leader>gc", "<cmd>G commit<cr>")
-	end,
-}
--- neovim/.config/nvim/lua/kbonnici/plugins/neogit.lua
+++ neovim/.config/nvim/lua/kbonnici/plugins/neogit.lua
@@ -5,5 +5,11 @@ return {
 		"sindrets/diffview.nvim", -- optional - Diff integration
 		"nvim-telescope/telescope.nvim", -- optional
 	},
-	config = true,
+	config = function()
+		local neogit = require("neogit")
+		neogit.setup()
+		vim.keymap.set("n", "<leader>gg", function()
+			neogit.open()
+		end)
+	end,
 }
