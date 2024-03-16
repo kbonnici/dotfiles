c422b0ccd76d2493e07d0a14a1846917b070abe8
Karl Bonnici <karl.bonnici100@gmail.com>
2024-03-15 23:00:10 -0700

    nvim: replace null-ls eslint diagnostics with lsp

--- neovim/.config/nvim/lua/kbonnici/plugins/none-ls.lua
+++ neovim/.config/nvim/lua/kbonnici/plugins/none-ls.lua
@@ -7,7 +7,6 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettierd,
-				null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.formatting.gofumpt,
 				null_ls.builtins.formatting.goimports_reviser,
 			},
