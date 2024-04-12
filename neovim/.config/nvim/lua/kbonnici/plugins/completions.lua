2ee418fe1280d4171a029a926fa79acde9cd0d44
Karl Bonnici <karl.bonnici100@gmail.com>
2024-04-12 12:41:36 -0700

    nvim:fix snippet mappings

--- neovim/.config/nvim/lua/kbonnici/plugins/completions.lua
+++ neovim/.config/nvim/lua/kbonnici/plugins/completions.lua
@@ -1,113 +1,119 @@
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  { "hrsh7th/cmp-buffer" },
-  { "hrsh7th/cmp-path" },
-  { "hrsh7th/cmp-cmdline" },
-  {
-    "hrsh7th/nvim-cmp",
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-      local cmp = require("cmp")
-      local ls = require("luasnip")
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+		build = "make install_jsregexp",
+	},
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-cmdline" },
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+			local cmp = require("cmp")
+			local ls = require("luasnip")
 
-      -- autocmd to forget snippet if you don't complete it
-      -- https://github.com/L3MON4D3/LuaSnip/issues/656#issuecomment-1313310146
-      local unlinkgrp = vim.api.nvim_create_augroup("UnlinkSnippetOnModeChange", { clear = true })
-      vim.api.nvim_create_autocmd("ModeChanged", {
-        group = unlinkgrp,
-        pattern = { "s:n", "i:*" },
-        desc = "Forget the current snippet when leaving the insert mode",
-        callback = function(evt)
-          if ls.session and ls.session.current_nodes[evt.buf] and not ls.session.jump_active then
-            ls.unlink_current()
-          end
-        end,
-      })
+			-- autocmd to forget snippet if you don't complete it
+			-- https://github.com/L3MON4D3/LuaSnip/issues/656#issuecomment-1313310146
+			local unlinkgrp = vim.api.nvim_create_augroup("UnlinkSnippetOnModeChange", { clear = true })
+			vim.api.nvim_create_autocmd("ModeChanged", {
+				group = unlinkgrp,
+				pattern = { "s:n", "i:*" },
+				desc = "Forget the current snippet when leaving the insert mode",
+				callback = function(evt)
+					if ls.session and ls.session.current_nodes[evt.buf] and not ls.session.jump_active then
+						ls.unlink_current()
+					end
+				end,
+			})
 
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0
-            and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<C-g>"] = cmp.mapping(function(fallback)
-            if ls.expand_or_jumpable() then
-              ls.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+			local has_words_before = function()
+				unpack = unpack or table.unpack
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<C-l>"] = cmp.mapping(function()
+						ls.jump(1)
+					end),
+					["<C-j>"] = cmp.mapping(function()
+						ls.jump(-1)
+					end),
+					["<C-g>"] = cmp.mapping(function(fallback)
+						if ls.expand_or_jumpable() then
+							ls.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif ls.jumpable(-1) then
-              ls.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        }, {
-          { name = "buffer" },
-        }),
-      })
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif ls.jumpable(-1) then
+							ls.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
 
-      -- Set configuration for specific filetype.
-      cmp.setup.filetype("gitcommit", {
-        sources = cmp.config.sources({
-          { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
-        }, {
-          { name = "buffer" },
-        }),
-      })
+			-- Set configuration for specific filetype.
+			cmp.setup.filetype("gitcommit", {
+				sources = cmp.config.sources({
+					{ name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+				}, {
+					{ name = "buffer" },
+				}),
+			})
 
-      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline({ "/", "?" }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = "buffer" },
-        },
-      })
+			-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
 
-      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          { name = "cmdline" },
-        }),
-      })
-    end,
-  },
+			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+			})
+		end,
+	},
 }
