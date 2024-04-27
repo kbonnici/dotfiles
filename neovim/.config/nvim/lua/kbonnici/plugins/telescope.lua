a3bc35fda252b7084d71a2097ba6874830e50793
Karl Bonnici <karl.bonnici100@gmail.com>
2024-04-26 17:40:32 -0700

    [nvim]: add help_tag mapping to telescope

--- neovim/.config/nvim/lua/kbonnici/plugins/telescope.lua
+++ neovim/.config/nvim/lua/kbonnici/plugins/telescope.lua
@@ -19,6 +19,7 @@ return {
 		config = function()
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files)
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags)
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep)
 			vim.keymap.set("n", "<C-p>", builtin.git_files)
 		end,
