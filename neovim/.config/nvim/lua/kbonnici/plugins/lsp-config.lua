a1a3b939c4c1c8ec3d9b174c95c955b43c66ef69
Karl Bonnici <karl.bonnici100@gmail.com>
2024-05-28 16:15:11 -0700

    more lspsaga bindings

--- neovim/.config/nvim/lua/kbonnici/plugins/lsp-config.lua
+++ neovim/.config/nvim/lua/kbonnici/plugins/lsp-config.lua
@@ -1,84 +1,90 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "html", "cssls" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "tsserver", "html", "cssls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-      require("mason-lspconfig").setup_handlers({
-        -- default handler
-        function(server_name)
-          lspconfig[server_name].setup({
-            capabilities = capabilities,
-          })
-        end,
-        -- Handlers for specific servers
-        ["lua_ls"] = function()
-          lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                workspace = {
-                  library = {
-                    vim.env.VIMRUNTIME .. "/lua",
-                  },
-                },
-              },
-            },
-          })
-        end,
-      })
+			require("mason-lspconfig").setup_handlers({
+				-- default handler
+				function(server_name)
+					lspconfig[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
+				-- Handlers for specific servers
+				["lua_ls"] = function()
+					lspconfig.lua_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								workspace = {
+									library = {
+										vim.env.VIMRUNTIME .. "/lua",
+									},
+								},
+							},
+						},
+					})
+				end,
+			})
 
-      vim.keymap.set("n", "<leader>gf", function()
-        vim.lsp.buf.format({ async = false })
-      end)
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        callback = function()
-          -- only format if an lsp is attached
-          if next(vim.lsp.get_clients()) ~= nil then
-            vim.lsp.buf.format({ async = false })
-          end
-        end,
-      })
-    end,
-    dependencies = {
-      {
-        "nvimdev/lspsaga.nvim",
-        config = function()
-          require("lspsaga").setup({
-            symbol_in_winbar = { -- breadcrumbs
-              enable = false,
-            },
-            lightbulb = {
-              enable = false,
-            },
-          })
+			vim.keymap.set("n", "<leader>gf", function()
+				vim.lsp.buf.format({ async = false })
+			end)
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				callback = function()
+					-- only format if an lsp is attached
+					if next(vim.lsp.get_clients()) ~= nil then
+						vim.lsp.buf.format({ async = false })
+					end
+				end,
+			})
+		end,
+		dependencies = {
+			{
+				"nvimdev/lspsaga.nvim",
+				config = function()
+					require("lspsaga").setup({
+						symbol_in_winbar = { -- breadcrumbs
+							enable = false,
+						},
+						lightbulb = {
+							enable = false,
+						},
+						finder = {
+							keys = {
+								toggle_or_open = "<cr>",
+							},
+						},
+					})
 
-          vim.keymap.set("n", "gr", ":Lspsaga finder ref<cr>")
-          vim.keymap.set("n", "gi", ":Lspsaga finder imp<cr>")
-          vim.keymap.set("n", "gd", ":Lspsaga goto_definition<cr>")
-          vim.keymap.set("n", "<leader>lr", ":Lspsaga rename<cr>")
-          vim.keymap.set("n", "<leader>lpr", ":Lspsaga lsp_rename ++project<cr>")
-        end,
-        dependencies = {
-          "nvim-treesitter/nvim-treesitter",
-          "nvim-tree/nvim-web-devicons",
-        },
-      },
-    },
-  },
+					vim.keymap.set("n", "gr", ":Lspsaga finder ref<cr>")
+					vim.keymap.set("n", "gi", ":Lspsaga finder imp<cr>")
+					vim.keymap.set("n", "gd", ":Lspsaga goto_definition<cr>")
+					vim.keymap.set("n", "<leader>lr", ":Lspsaga rename<cr>")
+					vim.keymap.set("n", "<leader>lpr", ":Lspsaga lsp_rename ++project<cr>")
+					vim.keymap.set({ "n", "v" }, "<leader>la", ":Lspsaga code_action<cr>")
+				end,
+				dependencies = {
+					"nvim-treesitter/nvim-treesitter",
+					"nvim-tree/nvim-web-devicons",
+				},
+			},
+		},
+	},
 }
