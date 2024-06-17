62f60de7bedf1530a67b6b580fb6ecae6c5ca91e
Karl Bonnici <karl.bonnici100@gmail.com>
2024-06-17 08:01:57 -0700

    nvim: remove rounded seperators in lualine

--- neovim/.config/nvim/lua/kbonnici/plugins/lualine.lua
+++ neovim/.config/nvim/lua/kbonnici/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
 		require("lualine").setup({
 			options = {
 				theme = "auto",
-				section_separators = { left = "", right = "" },
+				-- section_separators = { left = "", right = "" },
 			},
 		})
 	end,
