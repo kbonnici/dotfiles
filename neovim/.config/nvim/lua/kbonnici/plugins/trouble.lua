7618eba92f7c37720a16eeeb2b8375ae07565ce4
Karl Bonnici <karl.bonnici100@gmail.com>
2024-06-24 16:01:24 -0700

    nvim: rework trouble mappings

--- neovim/.config/nvim/lua/kbonnici/plugins/trouble.lua
+++ neovim/.config/nvim/lua/kbonnici/plugins/trouble.lua
@@ -1,42 +1,19 @@
 return {
-  "folke/trouble.nvim",
-  lazy = true,
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  config = function()
-    local trouble = require("trouble")
-    vim.keymap.set("n", "<leader>xx", trouble.toggle)
-    vim.keymap.set("n", "<leader>xn", function()
-      local hasNext = trouble.next({ skip_groups = true, jump = true })
-      if not hasNext then
-        print("[Trouble]: Nothing left to jump to.")
-      end
-    end)
-    vim.keymap.set("n", "<leader>xp", function()
-      local hasPrev = trouble.previous({ skip_groups = true, jump = true })
-      if not hasPrev then
-        print("[Trouble]: Nothing left to jump to.")
-      end
-    end)
-    vim.keymap.set("n", "<leader>xw", function()
-      print("Workspace Diagnostics")
-      trouble.toggle("workspace_diagnostics")
-    end)
-    vim.keymap.set("n", "<leader>xd", function()
-      print("Doc Diagnostics")
-      trouble.toggle("document_diagnostics")
-    end)
-    vim.keymap.set("n", "<leader>xq", function()
-      print("Quickfix List")
-      trouble.toggle("quickfix")
-    end)
-    vim.keymap.set("n", "<leader>xl", function()
-      print("Location List")
-      trouble.toggle("loclist")
-    end)
-  end,
+	"folke/trouble.nvim",
+	cmd = "Trouble",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {},
+	config = function()
+		local trouble = require("trouble")
+
+		local trouble_map = function(mapping, mode)
+			vim.keymap.set("n", mapping, function()
+				trouble.toggle({ mode = mode, focus = true })
+			end)
+		end
+
+		trouble_map("<leader>xx", "diagnostics")
+		trouble_map("<leader>xs", "lsp_document_symbols")
+		trouble_map("<leader>xl", "lsp")
+	end,
 }
