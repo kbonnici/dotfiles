bea64aaf5ec547cb9002dc6d65797e444e34ac4d
Karl Bonnici <karl.bonnici100@gmail.com>
2024-06-26 14:37:09 -0700

    nvim: add octo mapping

--- neovim/.config/nvim/lua/kbonnici/plugins/octo.lua
+++ neovim/.config/nvim/lua/kbonnici/plugins/octo.lua
@@ -7,9 +7,17 @@ return {
 	},
 	config = function()
 		require("octo").setup({
+			use_local_fs = true,
 			suppress_missing_scope = {
 				projects_v2 = true,
 			},
+			enable_builtin = true,
+			default_merge_method = "squash",
+			picker_config = {
+				use_emojis = true,
+			},
 		})
+
+		vim.keymap.set("n", "<leader>o", ":Octo<cr>")
 	end,
 }
