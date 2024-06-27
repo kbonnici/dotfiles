7ddc668ca32f8f00a792a759c23f73d70810d293
Karl Bonnici <karl.bonnici100@gmail.com>
2024-06-26 22:12:39 -0700

    nvim: install flash (holy sh*t this is awesome)

---
+++ neovim/.config/nvim/lua/kbonnici/plugins/flash.lua
@@ -0,0 +1,31 @@
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	opts = {},
+	keys = {
+		{
+			"<c-s>",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"S",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").treesitter()
+			end,
+			desc = "Flash Treesitter",
+		},
+		{
+			"s",
+			mode = { "o", "x" },
+			function()
+				require("flash").treesitter_search()
+			end,
+			desc = "Treesitter Search",
+		},
+	},
+}
