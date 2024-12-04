1b06a2a2f87629093e22dd49e57bec6b609bebae
Karl Bonnici <karl.bonnici100@gmail.com>
2024-12-03 20:58:23 -0800

    nvim: make lualine separators flat

--- neovim/.config/nvim/lua/kbonnici/plugins/lualine.lua
+++ neovim/.config/nvim/lua/kbonnici/plugins/lualine.lua
@@ -1,13 +1,14 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	event = "VeryLazy",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "auto",
-				-- section_separators = { left = "", right = "" },
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+      },
+    })
+  end,
 }
